fb3e8b9e6099428dca401bd2c3c05c15c6ebb54c
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-05-17 21:30:43 +0300

    SyncVar Callbacks

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar]
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+    
+    [SyncVar(hook = nameof(HandleDisplayNameTextUpdated))]
     [SerializeField]
     private string displayName = "Missing Name";
     
-    [SyncVar]
+    [SyncVar(hook = nameof(HandleDisplayColorUpdated))]
     [SerializeField]
     private Color displayColor = Color.black;
 
@@ -23,6 +27,16 @@ public class MyNetworkPlayer : NetworkBehaviour
     public void SetDisplayColor(Color newColor)
     {
         displayColor = newColor;
+    }
+
+    private void HandleDisplayNameTextUpdated(string oldText, string newText)
+    {
+        displayNameText.text = newText;
+    }
+
+    private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
     }
     
 }
